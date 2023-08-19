3eb1114a71142eb520f197f40fc1843a726e7b48
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-08-19 10:46:00 +0700

    git commit -m "update works_table.html" git push origin main

---
+++ asset/problem_code/p4.c
@@ -0,0 +1,75 @@
+#include "stdio.h"
+#include "string.h"
+
+int detect_pattern(const char *str){
+    if (strlen(str) < 2){
+        return 0;
+    }
+    char check[] = {str[0], str[1]}; // you can use 2 variables instead of array
+    for (int i = 2; i < strlen(str); i++){
+        if ((check[0] == '1') && (check[1] == '0')){
+            for (int j = i; j < strlen(str); j++){
+                if (str[j] == '1'){
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        check[0] = str[i - 1];
+        check[1] = str[i];
+    }
+    return 0;
+}
+
+//new detect pattern
+int detect_pattern2(const char *str){
+    char *start = str;
+    char *end = str + strlen(str) - 1;
+    while (start < end){
+        if ((*start == '1') && (*(start + 1) == '0')){
+            while (start < end){
+                if (*end == '1'){
+                    return 1;
+                }
+                end--;
+            }
+            return 0;
+        }
+        start++;
+    }
+}
+
+int detect_pattern3(const char *str){
+    int state = 0;
+    char *temp = str;
+    char *end = str + strlen(str) - 1;
+    while (state == 0 && temp <= end){
+        if (*temp == '1'){
+            state = 1;
+        }
+        temp++;
+    }
+    while (state == 1 && temp <= end){
+        if (*temp == '0'){
+            state = 2;
+        }
+        temp++;
+    }
+    while (state == 2 && temp <= end){
+        if (*temp == '1'){
+            return 1;
+        }
+        temp++;
+    }
+    return 0;
+}
+
+int main(){
+    char *test[]={ "","10","0100","011000","1010","0111001" };
+    int n = sizeof(test)/sizeof(char *);
+    for ( int i=0; i < n; i++ ) {
+        char *s = test[i];
+        printf( "'%s': %d\n", s, detect_pattern3(s) );
+    }
+    return 0;
+}
