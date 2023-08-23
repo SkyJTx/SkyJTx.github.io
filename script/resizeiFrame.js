6e45b2f4d00ff16a2e611823d19a66ae1b156da1
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-08-23 14:32:47 +0700

    git commit -m "recommit" git push origin main

--- script/resizeiFrame.js
+++
@@ -1,6 +0,0 @@
-function resizeIframe(obj) {
-    obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
-    var windowWidth = window.innerWidth;
-    var iframeDocument = obj.contentDocument || obj.contentWindow.document;
-    iframeDocument.body.style.fontSize = windowWidth / 50 + 'px';
-}
