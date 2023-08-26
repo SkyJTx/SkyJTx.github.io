143c26c9e3d35dec413a67f06f5266f58be6cf5e
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-08-26 13:25:20 +0700

    global navigation bar and footer and bordered content and markdown content

---
+++ script/constructor.js
@@ -0,0 +1,36 @@
+function createNavbar() {
+    const navbar = document.createElement('nav');
+    const navdiv = document.createElement('div');
+    navdiv.classList.add('navbar');
+
+    const homepageLink = document.createElement('a');
+    homepageLink.href = '/index.html';
+    homepageLink.textContent = 'Homepage';
+    navdiv.appendChild(homepageLink);
+
+    const worksTableLink = document.createElement('a');
+    worksTableLink.href = '/works_table.html';
+    worksTableLink.textContent = 'Works Table';
+    navdiv.appendChild(worksTableLink);
+
+    const aboutMeLink = document.createElement('a');
+    aboutMeLink.href = '/about.html';
+    aboutMeLink.textContent = 'About Me';
+    navdiv.appendChild(aboutMeLink);
+
+    navbar.appendChild(navdiv);
+
+    document.body.insertBefore(navbar, document.querySelector('.content'));
+}
+
+function createFooter() {
+    const hr = document.createElement('hr');
+    const footer = document.createElement('footer');
+
+    const footerParagraph = document.createElement('p');
+    footerParagraph.textContent = 'Version 0.3.0 | Mr. Nattakarn Khumsupha.';
+    footer.appendChild(footerParagraph);
+
+    document.body.appendChild(hr);
+    document.body.appendChild(footer);
+}
