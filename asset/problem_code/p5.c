4dd5e5a2f641b6d064abc78e26e4658e19c547d9
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2023-09-01 19:54:31 +0700

    Update p15678

--- asset/problem_code/p5.c
+++ asset/problem_code/p5.c
@@ -1,11 +1,1 @@
-<h1>Welcome to My Website</h1>
-<audio id="myAudio" autoplay style="display: none;">
-    <source src="https://archive.org/download/NeverGonnaGiveYouUp/jocofullinterview41.mp3" type="audio/mpeg">Your browser does not support the audio element.
-</audio>
-<script>
-    function playSoundOnLoad() {
-        var audio = document.getElementById("myAudio");
-        audio.play();
-    }
-    window.addEventListener("load", playSoundOnLoad);
-</script>
+https://ooooooooooooooooooooooo.ooo/ooooοооoοᴏοoοᴏοoοᴏooοᴏoᴏoᴏооoоᴏᴏoоᴏᴏοᴏοᴏοᴏοᴏοᴏοᴏoоᴏоοᴏоοοоᴏᴏοᴏοοοᴏοoοᴏοοοоoоοоοοoоᴏоοоoᴏοоᴏᴏοоᴏοoоᴏᴏοᴏοᴏοоoοοᴏοoοоoᴏοооooᴏᴏᴏοᴏοоoᴏᴏοοоοoοοoοοᴏοᴏoᴏοoοᴏοᴏoᴏоοοοοᴏοоοᴏοοоoοоoᴏοοoο
