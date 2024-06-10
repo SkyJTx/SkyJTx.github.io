877855200d2b8897b5460db9a80e93743dbe1e12
SkyJTx <111367081+SkyJTx@users.noreply.github.com>
2024-06-10 23:52:04 +0700

    add scrollcontroller

--- lib/presentation/homepage/home.dart
+++ lib/presentation/homepage/home.dart
@@ -6,107 +6,103 @@ import 'package:skyjtx_website/component/custom_scaffold.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  static const String routeKey = 'home';
+
   @override
   State<HomePage> createState() => HomePageState();
 }
 
 class HomePageState extends State<HomePage> {
-  final GlobalKey<AnimatedCircleAvatarState> animatedCircleAvatarKey =
-      GlobalKey<AnimatedCircleAvatarState>();
+  final GlobalKey<AnimatedCircleAvatarState> animatedCircleAvatarKey = GlobalKey();
+  final GlobalKey<CustomScaffoldState> customScaffoldKey = GlobalKey();
+  final scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return CustomScaffold(
+      key: customScaffoldKey,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height -
+        child: ListView(
+          controller: scrollController,
+          shrinkWrap: true,
+          children: [
+            SizedBox(
+              height: 100.h -
                   MediaQuery.of(context).padding.top -
                   MediaQuery.of(context).padding.bottom -
                   kToolbarHeight,
+              width: 100.w,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    AnimatedCircleAvatar(
+                      key: animatedCircleAvatarKey,
+                      backgroundColor: Colors.transparent,
+                      radius: 100,
+                      backgroundImage: const AssetImage('assets/icons/logo.jpg'),
+                      child: InkWell(
+                        focusColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                        overlayColor: WidgetStateProperty.all(Colors.transparent),
+                        customBorder: const CircleBorder(),
+                        onTap: () {
+                          animatedCircleAvatarKey.currentState?.controller.forward(from: 0);
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      'SkyJT\'s Website',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text(
+                      'Software Developer',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 100.h -
-                      MediaQuery.of(context).padding.top -
-                      MediaQuery.of(context).padding.bottom -
-                      kToolbarHeight,
-                  width: 100.w,
-                  child: Center(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        AnimatedCircleAvatar(
-                          key: animatedCircleAvatarKey,
-                          backgroundColor: Colors.transparent,
-                          radius: 100,
-                          backgroundImage: const AssetImage('assets/icons/logo.jpg'),
-                          child: InkWell(
-                            focusColor: Colors.transparent,
-                            highlightColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            overlayColor: WidgetStateProperty.all(Colors.transparent),
-                            customBorder: const CircleBorder(),
-                            onTap: () {
-                              animatedCircleAvatarKey.currentState?.controller.forward(from: 0);
-                            },
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        const SelectableText(
-                          'SkyJT\'s Website',
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SelectableText(
-                          'Software Developer',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ],
+            const SizedBox(height: 32),
+            Container(
+              margin: const EdgeInsets.all(32),
+              padding: const EdgeInsets.all(32),
+              decoration: BoxDecoration(
+                color: theme.colorScheme.primaryContainer.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Column(
+                children: [
+                  Text(
+                    'About Me',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                const SizedBox(height: 32),
-                Container(
-                  margin: const EdgeInsets.all(32),
-                  padding: const EdgeInsets.all(32),
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primaryContainer.withOpacity(0.5),
-                    borderRadius: BorderRadius.circular(8),
+                  SizedBox(height: 16),
+                  Text(
+                    'My name is Nattakarn Khumsupha. I\'m a computer engineering student at KMUTNB (King Mongkut\'s University of Technology North Bangkok). I\'m currently interested in Programming, Technology, Classical Music, History, and many more.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w300,
+                    ),
                   ),
-                  child: const Column(
-                    children: [
-                      SelectableText(
-                        'About Me',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      SelectableText(
-                        'My name is Nattakarn Khumsupha. I\'m a computer engineering student at KMUTNB (King Mongkut\'s University of Technology North Bangkok). I\'m currently interested in Programming, Technology, Classical Music, History, and many more.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
